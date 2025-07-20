57099106bb6879a06ceaf3eb716e4020a2223811
Vladimir Chmyr <vladimir.chmyr@gmail.com>
2025-07-20 17:00:56 +0300

    Init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <UApp>
+      <NuxtPage/>
+    </UApp>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+<UContainer class="flex flex-col gap-4">
+  <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'basis-1/3' }">
+    <img :src="item" width="234" height="234" class="rounded-lg">
+  </UCarousel>
+
+  <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'basis-1/3' }">
+    <img :src="item" width="234" height="234" class="rounded-lg">
+  </UCarousel>
+
+
+  <UCarousel v-slot="{ item }" :items="items" :ui="{ item: 'basis-1/3' }">
+    <img :src="item" width="234" height="234" class="rounded-lg">
+  </UCarousel>
+</UContainer>
+</template>
+
+<script setup lang="ts">
+const items = [
+  'https://picsum.photos/640/640?random=1',
+  'https://picsum.photos/640/640?random=2',
+  'https://picsum.photos/640/640?random=3',
+  'https://picsum.photos/640/640?random=4',
+  'https://picsum.photos/640/640?random=5',
+  'https://picsum.photos/640/640?random=6'
+]
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
